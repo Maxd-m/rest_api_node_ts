883159e61f61805202f85fa9d567f21bbd8078e5
MaxDM <maxdiazm.2004@gmail.com>
2025-11-25 09:47:11 -0600

    DB connection added

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,20 @@
 import express from "express";
 import router from "./router";
+import db from "./config/db";
+
+// conectar a db
+async function connectDB() {
+  try {
+    await db.authenticate()
+    db.sync()
+    console.log("Conexion exitosa a DB")
+  } catch (error) {
+    console.log(error)
+    console.log("error al conectarse a DB")
+  }
+}
+
+connectDB()
 
 const server = express()
 
