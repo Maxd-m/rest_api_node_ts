305e2f543ceae85ca8b98abb3151602ec741a19d
Angel Navarro <navarrojoseangel749@gmail.com>
2025-11-25 19:22:48 -0600

    feature: update products with put and patch

--- src/router.ts
+++ src/router.ts
@@ -1,47 +1,72 @@
-import {Router} from 'express'
-import {body , param} from 'express-validator'
-import { createProduct, getProductById, getProducts } from './handlers/product';
-import { handleInputErrors } from './middleware';
+import { Router } from "express";
+import { body, param } from "express-validator";
+import {
+  createProduct,
+  getProductById,
+  getProducts,
+  updateAvailability,
+  updateProduct,
+} from "./handlers/product";
+import { handleInputErrors } from "./middleware";
 
-const router=Router()
+const router = Router();
 
 // routing
 //              request (lo que se pide)
 //              response (lo que se obtiene)
 router.get("/", getProducts);
-router.get("/:id", 
-  param('id').isInt().withMessage('ID no válido'),
+router.get(
+  "/:id",
+  param("id").isInt().withMessage("ID no válido"),
   handleInputErrors,
   getProductById
 );
 
-router.post('/', 
+router.post(
+  "/",
   // validacion
-    body('name').notEmpty().withMessage('El nombre de producto no puede ser nulo'),
-    body("price")
-      .notEmpty()
-      .withMessage("El precio de producto no puede ser nulo")
-      .isNumeric()
-      .withMessage("Valor no valido")
-      .custom(value => value>0).withMessage('Precio no valido'),
-      handleInputErrors,
-      createProduct
-)
+  body("name")
+    .notEmpty()
+    .withMessage("El nombre de producto no puede ser nulo"),
+  body("price")
+    .notEmpty()
+    .withMessage("El precio de producto no puede ser nulo")
+    .isNumeric()
+    .withMessage("Valor no valido")
+    .custom((value) => value > 0)
+    .withMessage("Precio no valido"),
+  handleInputErrors,
+  createProduct
+);
 
 router.post("/", (req, res) => {
   res.json("Desde post");
 });
 
-router.put("/", (req, res) => {
-  res.json("Desde put");
-});
+router.put(
+  "/:id",
+  // validacion
+  body("name")
+    .notEmpty()
+    .withMessage("El nombre de producto no puede ser nulo"),
+  body("price")
+    .notEmpty()
+    .withMessage("El precio de producto no puede ser nulo")
+    .isNumeric()
+    .withMessage("Valor no valido")
+    .custom((value) => value > 0)
+    .withMessage("Precio no valido"),
+  body("availability")
+    .isBoolean()
+    .withMessage("El valor de availability no es valido"),
+  handleInputErrors,
+  updateProduct
+);
 
-router.patch("/", (req, res) => {
-  res.json("Desde patch");
-});
+router.patch("/:id", updateAvailability);
 
 router.delete("/", (req, res) => {
   res.json("Desde delete");
 });
 
-export default router
+export default router;
