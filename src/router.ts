a739564d25c00a0f92e68c31b8536fdb7381b85e
Angel Navarro <navarrojoseangel749@gmail.com>
2025-11-25 18:47:54 -0600

    feature: get products by id

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import {Router} from 'express'
-import {body} from 'express-validator'
-import { createProduct, getProducts } from './handlers/product';
+import {body , param} from 'express-validator'
+import { createProduct, getProductById, getProducts } from './handlers/product';
 import { handleInputErrors } from './middleware';
 
 const router=Router()
@@ -9,6 +9,11 @@ const router=Router()
 //              request (lo que se pide)
 //              response (lo que se obtiene)
 router.get("/", getProducts);
+router.get("/:id", 
+  param('id').isInt().withMessage('ID no válido'),
+  handleInputErrors,
+  getProductById
+);
 
 router.post('/', 
   // validacion
