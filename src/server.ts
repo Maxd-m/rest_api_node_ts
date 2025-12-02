330f8acec8626dac14b62e097e44b2e90a2f6c65
Erick <erickmm2710@gmail.com>
2025-12-02 11:27:05 -0600

    CORS Functionality

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import db from "./config/db";
 import colors from "colors"
 import swaggerUi from 'swagger-ui-express'
 import swaggerSpec from './config/swagger'
+import cors, {CorsOptions} from "cors";
+import morgan from "morgan";
 
 // conectar a db
 export async function connectDB() {
@@ -22,9 +24,24 @@ connectDB()
 // INstancia de express
 const server = express()
 
+// Permitir conexiones
+const corsOptions : CorsOptions = {
+    origin: function(origin, callback) {
+        if(origin === process.env.FRONTEND_URL) {
+            callback(null, true)
+        } else {
+            callback(new Error("No permitido por CORS"))
+        }
+    }
+}
+
+server.use(cors(corsOptions))
+
 // leer datos de formulario
 server.use(express.json())
 
+
+server.use(morgan('dev'))
 // se ejecuta en todos los request
 server.use('/api/products', router)
 
